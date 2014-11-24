c7c22e0fcfd330803e2dcacad81ff3f012e4bddd
Grady O'Connell <flipcoder@gmail.com>
2014-11-23 17:56:31 -0800

    major improvement on forking behavior

--- web.plugin.zsh
+++ web.plugin.zsh
@@ -3,28 +3,39 @@ function url-encode; {
     echo "${${(j: :)@}//(#b)(?)/%$[[##16]##${match[1]}]}"
 }
 
-:w() {
-    xdg-open "http://en.wikipedia.org/w/index.php?search=`url-encode "${(j: :)@}"`" &> /dev/null
+# run program, leaving terminal window open
+:() {
+    (setsid "$@" &> /dev/null &);
 }
-:yt() {
-    xdg-open "https://www.youtube.com/results?search_query=`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:g() {
-    xdg-open "https://www.google.com/search?q=`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:gi() {
-    xdg-open "http://www.duckduckgo.com/?q=!gi+`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:gh() {
-    xdg-open "https://github.com/search?q=`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:s() {
-    xdg-open "http://www.duckduckgo.com/?q=`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:d() {
-    xdg-open "http://www.duckduckgo.com/?q=!d+`url-encode "${(j: :)@}"`" &> /dev/null
-}
-:t() {
-    xdg-open "http://www.duckduckgo.com/?q=!t+`url-encode "${(j: :)@}"`" &> /dev/null
+# run program, closing current terminal on valid command
+::() {
+    type "$1" && (setsid "$@" &> /dev/null &) && exit;
 }
 
+export WEB_FORK_COMMAND=":"
+export #WEB_FORK_COMMAND="::"
+
+:w() {
+    $WEB_FORK_COMMAND xdg-open "http://en.wikipedia.org/w/index.php?search=`url-encode "${(j: :)@}"`"
+}
+:yt() {
+    $WEB_FORK_COMMAND xdg-open "https://www.youtube.com/results?search_query=`url-encode "${(j: :)@}"`"
+}
+:g() {
+    $WEB_FORK_COMMAND xdg-open "https://www.google.com/search?q=`url-encode "${(j: :)@}"`"
+}
+:gi() {
+    $WEB_FORK_COMMAND xdg-open "http://www.duckduckgo.com/?q=!gi+`url-encode "${(j: :)@}"`"
+}
+:gh() {
+    $WEB_FORK_COMMAND xdg-open "https://github.com/search?q=`url-encode "${(j: :)@}"`"
+}
+:s() {
+    $WEB_FORK_COMMAND xdg-open "http://www.duckduckgo.com/?q=`url-encode "${(j: :)@}"`"
+}
+:d() {
+    $WEB_FORK_COMMAND xdg-open "http://www.duckduckgo.com/?q=!d+`url-encode "${(j: :)@}"`"
+}
+:t() {
+    $WEB_FORK_COMMAND xdg-open "http://www.duckduckgo.com/?q=!t+`url-encode "${(j: :)@}"`"
+}
